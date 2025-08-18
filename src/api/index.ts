b781770d40469d4217cd5a0dbd1afebc3c35cf1b
Hiteshi09-mandloi <mandloihiteshi@gmail.com>
2025-08-18 14:30:40 +0530

    added url1

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:8091/api",
+  baseURL: process.env.REACT_APP_API_URL || "https://reporting-system-y96g.onrender.com/api",
   timeout: 60000,
 });
 
